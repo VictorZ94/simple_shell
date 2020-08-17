d48c72ee8183f03cf106d572ed3edb8de09388f0
camilo <camilo@pop-os.localdomain>
2020-08-17 12:28:51 -0500

    update

---
+++ test1/_strdup.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * _strdup - Duplicate a string in a new address memory
+ * @str: string to duplicate
+ *
+ * Return: pointer to char
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, j;
+
+	if (str == NULL)
+		return ('\0');
+
+	i = 0;
+	while (str[i])
+		i++;
+
+	p = malloc(sizeof(char) * i + 1); /* is not necesary to put space to NULL */
+	if (!p)
+		return ('\0');
+
+	j = 0;
+	while (j <= i)
+	{
+		p[j] = str[j];
+		j++;
+	}
+return (p);
+}
