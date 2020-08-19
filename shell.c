90587a8770b0ea47ffa20d6ec935fc0ddfa0b998
VictorZ94 <zrvictor@outlook.com>
2020-08-19 19:38:26 +0000

    betty style organized

--- shell.c
+++ shell.c
@@ -1,34 +1,33 @@
 #include "shell.h"
 /**
- * main
- * 
- * fix?
+ * main - our own shell
+ *
+ * Return: on succes 0, otherwise 1
  */
 int main(void)
 {
-    size_t len[BUFFSIZE];
-    ssize_t read = 0;
-    char *line = NULL, *directory = NULL;
-    char **readline;
-    int checkLine = 0;
+	size_t len[BUFFSIZE];
+	ssize_t read = 0;
+	char *line = NULL, *directory = NULL;
+	char **readline;
+	int checkLine = 0;
 
-    do
-    {
+	do {
 		if (isatty(STDIN_FILENO) == 1)
 			write(1, "$ ", 2);
-        read = getline(&line, len, stdin);
-        checkLine = verifyLine(read);
-        if (checkLine == 1)
-            break;
-        if (*line == '\n')
-            continue;
-        readline = _readline(line);
-        directory = startEnv(readline[0]);
-        _execve(directory, readline);
-        _freeargs(readline);
-        if (directory != NULL)
-            free (directory);
-    } while (isatty(STDIN_FILENO) == 1);
-    free(line);
-    return (0);
+		read = getline(&line, len, stdin);
+		checkLine = verifyLine(read);
+		if (checkLine == 1)
+			break;
+		if (*line == '\n')
+			continue;
+		readline = _readline(line);
+		directory = startEnv(readline[0]);
+		_execve(directory, readline);
+		_freeargs(readline);
+		if (directory != NULL)
+			free(directory);
+	} while (isatty(STDIN_FILENO) == 1);
+	free(line);
+	return (0);
 }
