fe7047c4368b54951d12239ad64fb1a98714138f
VictorZ94 <zrvictor@outlook.com>
2020-08-20 03:42:15 +0000

    string return length of a string

---
+++ _strlen.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strlen - my own function calculate the length of a string.
+ * @s: Argument - pointer to a type char.
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	return (i);
+}
