1175a2c488fd7f08fbcfc132d62c06711ba4098a
Lilsherlock <potatogames15@gmail.com>
2020-08-20 01:15:46 -0500

    update

--- shell.c
+++ shell.c
@@ -6,16 +6,22 @@
  */
 int main(void)
 {
-	size_t len[BUFFSIZE];
+	size_t len;
 	ssize_t read = 0;
 	char *line = NULL, *directory = NULL;
 	char **readline;
-	int checkLine = 0;
+	int checkLine = 0, flag = 0;
 
 	do {
+		if (flag == 1)
+		{
+			free(line);
+			flag = 0;
+		}
+		line = malloc(sizeof(char) * BUFFSIZE);
 		if (isatty(STDIN_FILENO) == 1)
 			write(1, "$ ", 2);
-		read = getline(&line, len, stdin);
+		read = getline(&line, &len, stdin);
 		checkLine = verifyLine(read);
 		if (checkLine == 1)
 			break;
@@ -24,6 +30,7 @@ int main(void)
 		readline = _readline(line);
 		directory = startEnv(readline[0]);
 		_execve(directory, readline);
+		flag = 1;
 		_freeargs(readline);
 	} while (isatty(STDIN_FILENO) == 1);
 	free(line);
