2cfac19688a606d8df30257456490ec32f0d9ad3
VictorZ94 <zrvictor@outlook.com>
2020-08-21 15:47:29 +0000

    to print character standard output

---
+++ _putchar.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * _putchar - print standard output char by char
+ * @c: character to print
+ *
+ * Return: standard output
+ */
+int _putchar(int c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - function that prints an string
+ * @s: string to print
+ *
+ * Return: always 0.
+ */
+int _puts(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (0);
+}
