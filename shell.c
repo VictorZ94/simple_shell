a74e077a9afbb56a3293389152083db1341448a6
VictorZ94 <zrvictor@outlook.com>
2020-08-21 01:15:40 +0000

    memory leaks controlled

--- shell.c
+++ shell.c
@@ -10,15 +10,9 @@ int main(void)
 	ssize_t read = 0;
 	char *line = NULL, *directory = NULL;
 	char **readline;
-	int checkLine = 0, flag = 0;
+	int checkLine = 0, check;
 
 	do {
-		if (flag == 1)
-		{
-			free(line);
-			flag = 0;
-		}
-		line = malloc(sizeof(char) * BUFFSIZE);
 		if (isatty(STDIN_FILENO) == 1)
 			write(1, "$ ", 2);
 		read = getline(&line, &len, stdin);
@@ -28,10 +22,16 @@ int main(void)
 		if (*line == '\n')
 			continue;
 		readline = _readline(line);
-		directory = startEnv(readline[0]);
+		check = PathCheck(readline[0]);
+		if (check == 0)
+			directory = readline[0];
+		else
+			directory = findPath(readline[0]);
+
 		_execve(directory, readline);
-		flag = 1;
 		_freeargs(readline);
+		if (directory != NULL && check != 0)
+			free (directory);
 	} while (isatty(STDIN_FILENO) == 1);
 	free(line);
 	return (0);
